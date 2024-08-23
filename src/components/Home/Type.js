8c01b1e13bffb563f3cccd77e0c0d58ddb340c1c
yesak <yesakgz12@gmail.com>
2024-08-23 11:30:34 +0300

    update resume

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,9 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
           "MERN Stack Developer",
+          " Tech Enthusiast",
+          "Freelancer",
           
         ],
         autoStart: true,
